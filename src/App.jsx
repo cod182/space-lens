7abf71b3c9f3fdce1e48ec64f18f2313fccd420c
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-07-31 17:16:01 +0100

    added three.js and added stars to background

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import { NavBar } from './components/index';
 import { Home, Rovers, Rover, Planet, Attribution } from './Routes/index';
 
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+
+
 import useStyles from './styles';
 
 const App = () => {
@@ -10,6 +14,7 @@ const App = () => {
 
   return (
     <div className={classes.root}>
+      
       <NavBar />
       <main className={classes.content}>
         <div className={classes.toolbar} />
@@ -24,6 +29,10 @@ const App = () => {
           </Route>
           <Route path="/attribution" element={<Attribution />} />
         </Routes>
+        <Canvas style={{ height: '100vh', width: '100vw', backgroundColor: 'black', zIndex: '-10', position: 'absolute', top: 0, left:0}}>
+          <ambientLight intensity={0.5} />
+          <Stars />
+      </Canvas>
       </main>
     </div>
   );
