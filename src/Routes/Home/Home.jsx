51e97382f8512ee9291c2c0dd03433579ab7fee1
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-08-02 16:58:09 +0100

    error handling for nasa apod api

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typography, Box } from '@mui/material';
 
 import useStyles from './styles';
 import { useGetAPODQuery } from '../../services/NASA';
 import { LoadingSpinner, FeaturedImage } from '../../components/index';
 
+import connectionIssues from '../../assets/images/connection.png';
+
 const Home = () => {
   let classes = useStyles();
 
   const { data, isFetching } = useGetAPODQuery({});
 
-  if (isFetching) {
+  const [dataFetching, setDataFetching] = useState(true);
+  const [signalLoss, setSignalLoss] = useState(false);
+
+  useEffect(() => {
+    const setSignalLost = () => {
+      setDataFetching(false);
+      setSignalLoss(true);
+    };
+    setTimeout(() => {
+      setSignalLost();
+    }, 10000);
+  }, []);
+
+  if (dataFetching) {
     return (
       <div className={classes.homeContainer}>
         <Typography align="center" variant="h2">
@@ -21,7 +36,21 @@ const Home = () => {
     );
   }
 
-  return (
+  return signalLoss ? (
+    <div className={classes.homeContainer}>
+      <Box className={classes.homeInfoContainer}>
+        <Typography variant="h3">Error: Signal Lost!</Typography>
+        <img
+          src={connectionIssues}
+          alt="Connection Issues"
+          style={{ maxWidth: '100%', height: '200px' }}
+        />
+        <Typography variant="body2">
+          Apologies, the NASA Api is experiencing connectivity issues.
+        </Typography>
+      </Box>
+    </div>
+  ) : (
     <div className={classes.homeContainer}>
       <FeaturedImage
         imgSrc={data.thumbnail_url ? data?.thumbnail_url : data?.url}
